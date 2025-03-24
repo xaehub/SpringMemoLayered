673e767cb7312afe5e3c5c5a907106726161d197
xaehub <novxae@gmail.com>
2025-03-24 10:31:09 +0900

    로직 분리하고 GetMapping 구현

--- src/main/java/com/example/layered/controller/MemoController.java
+++ src/main/java/com/example/layered/controller/MemoController.java
@@ -3,35 +3,36 @@ package com.example.layered.controller;
 import com.example.layered.dto.MemoRequestDto;
 import com.example.layered.dto.MemoResponseDto;
 import com.example.layered.entity.Memo;
+import com.example.layered.service.MemoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/memos")
 public class MemoController {
 
-    private final Map<Long, Memo> memoList = new HashMap<>();
+    private final MemoService memoService;
+
+    public MemoController(MemoService memoService) {
+        this.memoService = memoService;
+    }
 
     @PostMapping
     public ResponseEntity<MemoResponseDto> createMemo(@RequestBody MemoRequestDto dto) {
 
-        // 식별자가 1씩 증가 하도록 만듦
-        Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
+        // Service Layer 호출, 응답
+        return new ResponseEntity<>(memoService.saveMemo(dto), HttpStatus.CREATED);
+    }
 
-        // 요청받은 데이터로 Memo 객체 생성
-        Memo memo = new Memo(memoId, dto.getTitle(), dto.getContents());
+    @GetMapping
+    public List<MemoResponseDto> findAllMemos() {
 
-        // Inmemory DB에 Memo 메모
-        memoList.put(memoId, memo);
-
-        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.CREATED);
+        return  memoService.findAllMemos();
     }
 }
---
+++ src/main/java/com/example/layered/service/MemoServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.layered.service;
+
+import com.example.layered.dto.MemoRequestDto;
+import com.example.layered.dto.MemoResponseDto;
+import com.example.layered.entity.Memo;
+import com.example.layered.repository.MemoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Annotation @Service는 @Component와 같다, Spring Bean으로 등록한다는 뜻.
+ * Spring Bean으로 등록되면 다른 클래스에서 주입하여 사용할 수 있다.
+ * 명시적으로 Service Layer 라는것을 나타낸다.
+ * 비지니스 로직을 수행한다.
+ */
+@Service
+public class MemoServiceImpl implements MemoService{
+
+    private final MemoRepository memoRepository;
+
+    public MemoServiceImpl(MemoRepository memoRepository) {
+        this.memoRepository = memoRepository;
+    }
+
+    @Override
+    public MemoResponseDto saveMemo(MemoRequestDto dto) {
+
+        // 요청받은 데이터로 MEMO 객체 생성 ID 없음
+        Memo memo = new Memo(dto.getTitle(), dto.getContents());
+
+        // DB 저장
+        Memo savedMemo = memoRepository.saveMemo(memo);
+
+        return new MemoResponseDto(savedMemo);
+
+    }
+
+    @Override
+    public List<MemoResponseDto> findAllMemos() {
+
+        return memoRepository.findAllMemos();
+    }
+}
